abdf855edfcc8fac1eea95d8da0f73e8db544013
jy846366379 <yi.ji@vanderbilt.edu>
2020-09-01 14:29:37 +0800

    1st commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <footer className="footer">
+                (c)2020 StarLink Tracker. All Rights Reserved. Website Made by Yi Ji
+            </footer>
+        );
+    }
+}
+
+export default Footer;
